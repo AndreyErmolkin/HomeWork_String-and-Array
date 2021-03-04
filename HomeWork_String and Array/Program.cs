14f6d6844b7eb7c867455f76f45fa805609969da
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-03-04 21:17:50 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_String and Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace HomeWork_String_and_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[8] { 7, -65, 87, 348, -734, -43, 724, 652 };
+            
+            foreach (int i in numbers)
+            {
+                
+                if (i <= 0)
+                {
+                   
+                    Console.WriteLine("Отрицательное число =" + i); 
+                }
+
+
+              
+            }
+            int lastMin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Номер числа:"+ Array.IndexOf(numbers, lastMin));
+            
+
+        }
+
+
+    }
+}
